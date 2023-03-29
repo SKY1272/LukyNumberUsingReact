4dbfbb8ea2aea5b0609f31f9873c6bd87debfc34
SKY <88556477+SKY1272@users.noreply.github.com>
2023-03-29 17:09:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import LukyNumber from "./components/LukyNumber";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <LukyNumber />
+    </div>
+  );
+}
